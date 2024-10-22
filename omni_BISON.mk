7c60fe94fe442bd3d5131971f68cd9413820b7bf
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-22 11:03:38 +0300

    Update omni_BISON.mk

--- omni_BISON.mk
+++ omni_BISON.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/hxy/BISON/device.mk)
 
 PRODUCT_DEVICE := BISON
-PRODUCT_NAME := omni_BISON
+PRODUCT_NAME := twrp_BISON
 PRODUCT_BRAND := UMIDIGI
 PRODUCT_MODEL := BISON
 PRODUCT_MANUFACTURER := hxy
