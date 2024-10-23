e4cb943f08d9c9bb5e990688733277b5e5915a34
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-23 09:22:53 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
 
+# Otacert
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(DEVICE_PATH)/security/releasekey
+
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
