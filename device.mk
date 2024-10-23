69a213ca9ffb6f33bc52442733ed75ce2bad05b2
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-23 17:42:10 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -47,11 +47,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# Mtk plpath utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
