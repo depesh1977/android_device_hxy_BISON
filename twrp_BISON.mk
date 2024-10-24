0bedd1fcc548c46d49a1512babdaa10f4206c873
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-24 12:27:59 +0300

    Update twrp_BISON.mk

--- twrp_BISON.mk
+++ twrp_BISON.mk
@@ -8,6 +8,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Inherit from BISON device
 $(call inherit-product, device/hxy/BISON/device.mk)
 
