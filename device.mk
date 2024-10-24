e115362d0ae74e47f044ce92bd24c9b241292004
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-10-24 12:27:02 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,7 +16,6 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
